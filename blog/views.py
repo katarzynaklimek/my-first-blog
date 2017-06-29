f80751064b20462cab9a3795979a988c9f8e1032
Katarzyna Klimek <katarzyna.monika.klimek@gmail.com>
2017-06-29 11:52:44 +0200

    dodawanie postów

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Post
 from .forms import PostForm
+
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
--- mysite/urls.py
+++ mysite/urls.py
@@ -4,4 +4,6 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'', include('blog.urls')),
+    url(r'^i18n/', include('django.conf.urls.i18n')),
+
 ]
